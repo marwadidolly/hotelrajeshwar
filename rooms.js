8f9ccc479096e97eb7404ae5a1a90d9916b42d12
marwadidolly <marwadidolly20@gmail.com>
2025-07-02 15:06:20 +0530

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,15 +1,6 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  const roomCards = document.querySelectorAll(".room-card");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  }, {
-    threshold: 0.1
+  const lightbox = GLightbox({
+    selector: '.glightbox'
   });
-
-  roomCards.forEach(card => observer.observe(card));
 });
