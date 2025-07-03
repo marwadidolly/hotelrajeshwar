012a3e2beff04e32814ca02640137bd716cf4c62
marwadidolly <marwadidolly20@gmail.com>
2025-07-03 21:54:16 +0530

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,6 +1,5 @@
 
 function toggleMenu() {
-  const nav = document.querySelector(".nav-links");
-  nav.classList.toggle("active");
+  const menu = document.querySelector('.nav-links');
+  menu.classList.toggle('active');
 }
-
