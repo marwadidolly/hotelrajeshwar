362708d0e4cb9018fd051604579a4e58b8e9024f
marwadidolly <marwadidolly20@gmail.com>
2025-07-03 16:49:30 +0530

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,6 +1,11 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const lightbox = GLightbox({
-    selector: '.glightbox'
-  });
+// Initialize lightbox for room images
+const lightbox = GLightbox({
+  selector: '.glightbox',
 });
+
+// For mobile navbar toggle
+function toggleMenu() {
+  const menu = document.querySelector('.nav-links');
+  menu.classList.toggle('active');
+}
